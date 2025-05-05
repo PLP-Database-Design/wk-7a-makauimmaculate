2011fec1e628d0c19e321409e74ca289d4bf6aa2
makauimmaculate <151067818+makauimmaculate@users.noreply.github.com>
2025-05-05 19:57:06 +0300

    corrected answers.sql

--- answers.sql
+++ answers.sql
@@ -1,24 +1,43 @@
--- Question 1
--- Creating the 1NF version
 
-SELECT 101 AS OrderID, 'John Doe' AS CustomerName, 'Laptop' AS Product
-UNION ALL
-SELECT 101, 'John Doe', 'Mouse'
-UNION ALL
-SELECT 102, 'Jane Smith', 'Tablet'
-UNION ALL
-SELECT 102, 'Jane Smith', 'Keyboard'
-UNION ALL
-SELECT 102, 'Jane Smith', 'Mouse'
-UNION ALL
-SELECT 103, 'Emily Clark', 'Phone';
+ --  Question 1
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
 
 -- Question 2
--- Table 1: Orders (OrderID -> CustomerName)
-SELECT DISTINCT OrderID, CustomerName
-FROM OrderDetails;
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
--- Table 2: OrderLineItems (OrderID + Product -> Quantity)
-SELECT OrderID, Product, Quantity
-FROM OrderDetails;
+CREATE TABLE Product (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
 
